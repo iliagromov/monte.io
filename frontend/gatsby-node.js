b85abc0ed8b92a70108b7edc0053d278f3968355
iliagromov <ilia.workproffile@gmail.com>
2023-04-27 17:17:56 +0300

    Сделал запрос для новостей

--- frontend/gatsby-node.js
+++ frontend/gatsby-node.js
@@ -57,29 +57,85 @@ exports.createPages = async ({ graphql, actions: { createPage }  }) => {
     });
   });
   await graphql(`
-     {
-      allWpPage {
-          nodes {
-            id
-            uri
-            slug
-            title
-            content
+    {
+      allWpPost {
+        nodes {
+          id  
+          slug
+          uri
+          title
+          content
+          categories {
+            nodes {
+              id
+              slug
+              name
+            }
           }
         }
-     }
-   `).then(result => {
-    // console.log(result.data.allWpPage.nodes);
-    result.data.allWpPage.nodes.forEach(node => {
-      createPage({
-        path: `${node.uri}`,
-        component: path.resolve(`./src/templates/page-single-news.tsx`),
-        context: {
-          // This is the $slug variable passed to blog-post.js
-          uri: node.uri,
-          slug: node.slug,
-        },
-      });
+      }
+    }`)
+    .then(result => {
+      result.data.allWpPost.nodes.forEach(node => {
+        console.log(node);
+        createPage({
+          path: `${node.slug}`,
+          component: path.resolve(`./src/templates/page-single-news.tsx`),
+          context: {
+            // This is the $slug variable passed to blog-post.js
+            slug: node.slug,
+            uri: node.uri,
+            name: node.name,
+            categories: node.name,
+          },
+        });
+      })
     });
-  });
+  // await graphql(`
+  //    {
+  //     allWpCategory {
+  //       edges {
+  //         node {
+  //           id
+  //           name
+  //           posts {
+  //             nodes {
+  //               id
+  //               title
+  //               uri
+  //               slug
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //    }
+  //  `).then(result => {
+  //   // console.log('result.data');
+  //   // console.log(result.data.allWpCategory.edges);
+  //   result.data.allWpCategory.edges.forEach(edge => {
+  //     console.log(edge.node.name);
+  //     createPage({
+  //       path: `categories/${edge.node.name}`,
+  //       component: path.resolve(`./src/templates/page-single-news.tsx`),
+  //       context: {
+  //         // This is the $slug variable passed to blog-post.js
+  //         uri: edge.node.name,
+  //       },
+  //     });
+
+  //     edge.node.posts.nodes.forEach(node =>{
+  //       console.log(node);
+  //       createPage({
+  //         path: `posts/${node.slug}`,
+  //         component: path.resolve(`./src/templates/page-single-news.tsx`),
+  //         context: {
+  //           // This is the $slug variable passed to blog-post.js
+  //           uri: node.name,
+  //           slug: node.slug,
+  //         },
+  //       });
+  //     });
+  //   });
+  // });
 }
--- frontend/src/templates/page-single-news.tsx
+++ frontend/src/templates/page-single-news.tsx
@@ -2,19 +2,37 @@ import React from "react";
 import Layout from "../layouts/Default/en";
 import { Link, graphql, useStaticQuery } from "gatsby";
 import { PageProps } from 'gatsby'
+
+
+/**
+ * Я могу получать все посты и фильтровать их по нужным параметрам и выводить
+ * если мне нужны категории я могу получать все категории сравнивать с названием и выводить
+ * Если запрос сделан на посты, то тут тоже дергаю посты, чтобы не путаться, потому подумать как правильно выполять запросы WP
+ */
 export const query = graphql`
   query($slug: String!) {
-    allWpPage(filter: { slug: { eq: $slug } }) {
+    allWpPost(filter: { slug: { eq: $slug } }) {
       nodes {
+        id  
+        slug
+        uri
         title
         content
+        categories {
+          nodes {
+              id
+              slug
+              name
+            }
+        }
       }
     }
   }
 `;
 
 export default function BlogPost({ data, location }) {
-  const post = data.allWpPage.nodes[0];
+  // FIXME: data.allWpPost.nodes[0] cделать нормальный путь и проверку
+  const post = data.allWpPost.nodes[0];
   console.log(post);
   return (
     <Layout location={location}>
